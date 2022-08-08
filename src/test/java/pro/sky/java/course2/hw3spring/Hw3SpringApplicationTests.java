5f8542cb999a644e61f2c8145abef124bace8310
Anastasia <shvayka92@mail.ru>
2022-08-08 21:39:31 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/hw3spring/Hw3SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.hw3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hw3SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
